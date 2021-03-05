18167d8808b9c748e31331703173f54dd1a70ff3
Bill Caffery <billythakidd04@gmail.com>
2021-03-05 15:12:48 -0500

    added nav.php and included on every page

--- media.php
+++ media.php
@@ -5,6 +5,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 <body>
+	<? include 'nav.php';?>
 -.pdf of article
 	-gallery of images from Vegas and WC
 	-any media/interactable things i can scrape from KDC
