26e42242eb5fc3c5627bb0d75efbbd3d4840d8c8
rbaia28 <baia5230@gmail.com>
2021-03-08 14:37:53 -0500

    updated navbar

--- nav.php
+++ nav.php
@@ -1,9 +1,15 @@
 
 	<header class="navbar sticky-top navbar-light bg-light">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="#">
+			<a class="navbar-brand" href= "index.php">:D</a>
 				<img src="img/newlogo.jpg" alt="" width="30" height="24" class="d-inline-block align-top">
-				Bootstrap
+				<li class="nav-item">
+					<a class="nav-link" href="services.php">Services</a>
+				</li>				<li class="nav-item">
+					<a class="nav-link" href="media.php">Media</a>
+				</li>				<li class="nav-item">
+					<a class="nav-link" href="projects.php">Projects</a>
+				</li>
 			</a>
 		</div>
 	</header>
