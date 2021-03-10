a3dacf1c74e66d417f5e663cfc7f39e687a984b5
rbaia28 <baia5230@gmail.com>
2021-03-10 14:16:33 -0500

    added a js powered random fact generator

---
+++ randomfacts.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>A look inside my brain..</title>
+</head>
+<body>
+	<h1>Random Fact Generator!</h1>
+	<div id="factDisplay">
+	</div>
+	<button onclick="newFact()"> New Quote </button>
+
+	<script src="javascript.js"></script>
+</body>
+</html>
