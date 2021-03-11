dbcf995445049884de4672b07e308ef1b136fc64
rbaia28 <baia5230@gmail.com>
2021-03-11 10:57:03 -0500

    fixed javascript to actually post quote

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-var facts = [
-"naked mole rat" 
-"edc facts"
-"milk facts"
-"dime facts"
-"cat facts"
-"vegas facts"
+var fact= [
+"naked mole rat",
+"edc facts",
+"milk facts",
+"dime facts",
+"cat facts",
+"vegas facts",
 "science facts" 
 ]
 
 function newFact() {
     var randomNumber = Math.floor(Math.random()*(fact.length));
-    document.getElementById('factDisplay').innerHTML = facts[randomNumber];
+    document.getElementById('factDisplay').innerHTML = fact[randomNumber];
 }
 //https://www.freecodecamp.org/news/creating-a-bare-bones-quote-generator-with-javascript-and-html-for-absolute-beginners-5264e1725f08/
